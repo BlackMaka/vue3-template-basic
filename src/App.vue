bfeac9065845c9540181e128aa9841a27c238dd8
completedLee <completed0728@gmail.com>
2021-10-12 22:52:20 +0900

    computed get(), set()

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,42 @@
 <template>
-  <!-- <h1>{{ msg + '??' }}</h1> -->
-  
-  <!-- 
-    로직이 너무 많이 실행된다
-  <h1>{{ msg.split('').reverse().join('') }}</h1>
-  <h1>{{ msg.split('').reverse().join('') }}</h1>
-  <h1>{{ msg.split('').reverse().join('') }}</h1> 
-  -->
-  <!-- 
-    로직이 너무 많이 실행된다
-  <h1>{{ reveseMessage() }}</h1>
-  <h1>{{ reveseMessage() }}</h1>
-  <h1>{{ reveseMessage() }}</h1>
-  -->
-
-  <!-- 이미 계산되서 한번만 연산하고 나머지 [캐싱] 처리한다. -->
-  <h1>{{ computedMessage }}</h1>
-  <h1>{{ computedMessage }}</h1>
-  <h1>{{ computedMessage }}</h1>
+  <button @click="add">
+    ADD
+  </button>
+  <h1>{{ revesedMessage }}</h1>
+  <h1>{{ revesedMessage }}</h1>
+  <h1>{{ revesedMessage }}</h1>
 </template>
 
 <script>
 export default {
   data(){
     return {
+      //getter, setter
       msg : 'Hello Computed'
     }
   },
   methods:{
-    reveseMessage(){
-      return this.msg.split('').reverse().join('');
+    add(){ 
+      //getter
+      //readonly 읽기 전용이라 바뀌지 않는다. 
+      //this.revesedMessage += '!?'
+      
+      //getter, setter 값이 바뀐다.
+      this.revesedMessage2 += '!?'
     }
   },
   computed:{
     revesedMessage(){
       return this.msg.split('').reverse().join('');
+    },
+    //getter, setter
+    revesedMessage2:{
+      get(){
+        return this.msg.split('').reverse().join('');
+      },
+      set(value){
+        this.msg = value;
+      }
     }
   }
 }
