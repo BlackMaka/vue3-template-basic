5d10e66396c662b47c90989701402af3c19ab419
completedLee <completed0728@gmail.com>
2021-10-12 22:59:01 +0900

    watch 변경감지

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <h1 @click="changeMessage">
+    {{ msg }}
+  </h1>
+  <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      message: 'Hello Vue!'
+  data(){
+    return{
+      msg : 'Hello?'
     }
   },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
+  computed:{
+    reversedMessage(){
+      return this.msg.split('').reverse().join('');
+    }
+  },
+  watch:{ 
+    //msg 데이터 변경 감시
+    msg(newValue, oldValue){
+      console.log(newValue); //Good!
+      console.log(oldValue); //Hello?
+    },
+    //계산된 값도 변경이 있으면 감지가 가능하다.
+    reversedMessage(newValue, oldValue){
+      console.log(newValue); //!dooG
+      console.log(oldValue); //?olleH
+    }
+  },
+  methods:{
+    changeMessage(){
+      this.msg = 'Good!';
     }
   }
 }
 </script>
+
+<style>
+
+</style>
