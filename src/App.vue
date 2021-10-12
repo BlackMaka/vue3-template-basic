e2b4b36d14ec1189a24be9345a5fce5fe9cabfb8
completedLee <completed0728@gmail.com>
2021-10-12 22:42:10 +0900

    computed caching

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <!-- <h1>{{ msg + '??' }}</h1> -->
+  
+  <!-- 
+    로직이 너무 많이 실행된다
+  <h1>{{ msg.split('').reverse().join('') }}</h1>
+  <h1>{{ msg.split('').reverse().join('') }}</h1>
+  <h1>{{ msg.split('').reverse().join('') }}</h1> 
+  -->
+  <!-- 
+    로직이 너무 많이 실행된다
+  <h1>{{ reveseMessage() }}</h1>
+  <h1>{{ reveseMessage() }}</h1>
+  <h1>{{ reveseMessage() }}</h1>
+  -->
+
+  <!-- 이미 계산되서 한번만 연산하고 나머지 [캐싱] 처리한다. -->
+  <h1>{{ computedMessage }}</h1>
+  <h1>{{ computedMessage }}</h1>
+  <h1>{{ computedMessage }}</h1>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
-  components: {
-    HelloWorld
-  },
-  data() {
+  data(){
     return {
-      message: 'Hello Vue!'
+      msg : 'Hello Computed'
     }
   },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
+  methods:{
+    reveseMessage(){
+      return this.msg.split('').reverse().join('');
+    }
+  },
+  computed:{
+    revesedMessage(){
+      return this.msg.split('').reverse().join('');
     }
   }
 }
 </script>
+
+<style>
+
+</style>
