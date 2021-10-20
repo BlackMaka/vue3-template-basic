919f2a245e90dbdfe7e9da7797bd6d22fffbeabf
suhan <completed0728@gmail.com>
2021-10-20 13:28:37 +0900

    이벤트 핸들링 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <div>
+    <button @click="handler('dd',$event)"></button>
+    <button @click="handlerA">
+      ()생략가능
+    </button>
+    <button @click="handlerA(), handlerB()">
+      ()를 입력한다
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
+  methods:{
+    handler(msg, event){
+      console.log(msg, event);
+    },
+    handlerA(){
+      console.log('A');
+    },
+    handlerB(){
+      console.log('B');
     }
   }
 }
 </script>
+
+<style>
+
+</style>
