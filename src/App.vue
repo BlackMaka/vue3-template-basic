74e98c6e93bc4e1ccccc5e3d01a5861c0493291d
suhan <completed0728@gmail.com>
2021-10-20 18:06:18 +0900

    ref this.  하위컴포넌트 루트및 루트아닐때 ref 요소 찾기

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <div>
+    <h1 id="hello1">
+      hello1
+    </h1>
+    <h1 ref="hello2">
+      hello2
+    </h1>
+    <Hello ref="hello3" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
+import Hello from '~/components/Hello'
 export default {
-  components: {
-    HelloWorld
+  components:{
+    Hello
   },
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
+  mounted(){
+    console.log('mounted');
+    const hello1 = document.querySelector('#hello1');
+    console.log(hello1.textContent); // hello1
+    console.log(this.$refs.hello2.textContent); // hello2
+    //console.log(this.$refs.hello3.$el.textContent); // hello3 상위 루트가 하나일때만 가능
+    console.log(this.$refs.hello3.$refs.hello32.textContent); 
   },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
-    }
-  }
+  
 }
 </script>
+
+<style>
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>hello3</div>
+    <div ref="hello32">
+      hello3-2
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
