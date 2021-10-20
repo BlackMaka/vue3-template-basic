478833acdb38261dba193df00bc5d96186dca0be
suhan <completed0728@gmail.com>
2021-10-20 11:55:56 +0900

    list shortid, 배열변이메소드

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div>
-    <!--v-if-->
-    <h1 v-if="false">
-    </h1>
-    <!-- style="display:none" -->
-    <h1 v-show="false">
-    </h1>
+    <button @click="handler">
+      btn
+    </button>
+    <ul>
+      <li
+        v-for="{name, id} in newFruits"
+        :key="id">
+        {{ name }} - {{ id }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+//npm i -D shortid
+import shortid from 'shortid'
 export default {
-  //v-if 게으름 : false 구조적으로 렌더링 하지않음  
-  //전환비용이 크다 DOM 구조가 리렌더링 되기 때문에
-  //v-show : false DOM에 남아있음 /자주 쓰는건 v-show (구조적으로 이미 렌더링이 되어있음 초기 렌더링 비용이 크다 )
-  // 전환비용이 낮음 disply none으로만 바꿔주면됨
+  data (){
+    return {
+      fruits:['apple', 'banana', 'cherry']
+    }
+  },
+  computed:{
+    newFruits(){
+      return this.fruits.map(fruit =>({
+          id: shortid.generate(),
+          name:fruit
+        }))
+    }
+  },
+  methods:{
+    handler(){
+      this.fruits.push('orange');
+    }
+  }
 }
 </script>
 
