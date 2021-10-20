49352201867996bed831e9c6f4f9b9dd92d6bf75
suhan <completed0728@gmail.com>
2021-10-20 16:45:08 +0900

    component 기본 props, slot

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <div>
+    <MyBtn>
+      <span style="color:red">Apple</span>
+    </MyBtn>
+    <MyBtn color="royalblue">
+      Banana
+    </MyBtn>
+    <MyBtn
+      large
+      :color="color">
+      Cherry
+    </MyBtn>
+    <!-- :바인딩 차이점은 숫자로 타입까지 인식가능 -->
+    
+    <MyBtn>Orange</MyBtn>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
+import MyBtn from '~/components/MyBtn'
 export default {
-  components: {
-    HelloWorld
+  components:{
+    MyBtn
   },
-  data() {
+  data(){
     return {
-      message: 'Hello Vue!'
-    }
-  },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
+      color:'#000'
     }
   }
 }
 </script>
+
+<style>
+
+</style>
---
+++ src/components/MyBtn.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="btn"
+    :class="{large}"
+    :style="{ backgroundColor: color}">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        color:{
+            type:String,
+            default:'gray'
+        },
+        large:{
+            type:Boolean,
+            default:false
+        }
+    }
+
+}
+</script>
+
+<style lang=scss>
+    .btn{
+        border-radius: 5px;
+        display: inline-block;
+        color: white;
+        background-color: gray;
+        padding: 10px;
+        margin: 3px;
+        &.large{
+            padding: 30px;
+        }
+    }
+</style>
