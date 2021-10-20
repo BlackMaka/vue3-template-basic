83d3be5e0f339419259bf96c6ed00188b2602a36
suhan <completed0728@gmail.com>
2021-10-20 15:51:10 +0900

    폼입력 단방향, 양방향바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <div>
+    {{ msg }}
+    <input 
+      type="text" 
+      :value="msg"
+      @input="handler" />
+    <input 
+      type="text" 
+      :value="msg"
+      @input="msg = $event.target.value" />
+    <!-- 한글은 한박자 느리기때문에 두번째걸 사용 -->
+    <input 
+      type="text" 
+      v-model="msg" />
+    <br />
+    {{ isChecked }}
+    <input
+      type="checkbox" 
+      v-model="isChecked" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      message: 'Hello Vue!'
+  data(){
+    return{
+      msg : 'Hello World!',
+      isChecked:false
     }
   },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
+  methods:{
+    handler(event){
+      this.msg = event.target.value;
     }
   }
 }
 </script>
+
+<style>
+
+</style>
