69e718475b5fc0d21218654607be3f24eb3365cf
suhan <completed0728@gmail.com>
2021-10-20 15:58:25 +0900

    v-model 수식어

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <div>
+    <h1>{{ msg }}</h1>
+    <input
+      type="text"
+      :value="msg"
+      @chage="msg = $event.target.value" />
+    <input
+      type="text" 
+      v-model.lazy="msg" />
+    <!-- =========================================== -->
+    <input
+      type="text"
+      v-model.number="num" />
+    <!-- number type유지 -->
+    <input
+      type="text"
+      v-model.trim="msg" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      message: 'Hello Vue!'
+  data(){
+    return{
+      msg: 'Hello World!',
+      num :123,
     }
   },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
+  watch:{
+    msg(){
+      console.log(typeof this.num);
     }
   }
 }
 </script>
+
+<style>
+
+</style>
